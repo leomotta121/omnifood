81fad5b5903a0a694814dac0c40275b619dfbce8
Leonardo Motta <leo@domboscoatacado.com.br>
2018-06-02 21:28:46 -0300

    buttons scroll implemented

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    /* For sticky navigation */
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -9,4 +10,20 @@ $(document).ready(function () {
     }, {
         offset: '60px;'
     });
+
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
+    });
+
+    /* Navigation scroll */
+    
 });
